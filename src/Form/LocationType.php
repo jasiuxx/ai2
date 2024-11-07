7f21f6613c914d956e34ca960f3b7cab603480a1
Jan Szczudło <sj51476>
2024-11-07 15:31:13 +0100

    lab-d

---
+++ src/Form/LocationType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('city', null, [
+                'attr' => ['placeholder' => 'Enter city name'],
+            ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland' => 'PL',
+                    'Germany' => 'DE',
+                    'France' => 'FR',
+                    'Spain' => 'ES',
+                    'Italy' => 'IT',
+                    'United Kingdom' => 'GB',
+                    'United States' => 'US',
+                ],
+            ])
+            ->add('latitude', NumberType::class, [
+                'attr' => ['placeholder' => 'Enter latitude'],
+                'label' => 'Latitude',
+            ])
+            ->add('longitude', NumberType::class, [
+                'attr' => ['placeholder' => 'Enter longitude'],
+                'label' => 'Longitude',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+            'validation_groups' => ['Default'], // Domyślna grupa walidacyjna
+        ]);
+    }
+}
